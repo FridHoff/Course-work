b998cd3bacd5156bcfff31f4a710f8162ad337bf
Admin <Admin@MAIN>
2025-12-15 12:22:15 +0300

    Fixed some bugs

--- Course work/Course work.cpp
+++ Course work/Course work.cpp
@@ -19,22 +19,129 @@ struct Computer
 	int memory_count;
 	float price;
 	Computer* prev, * next;
+	//Запускает процесс ввода нового ПК
+	void CompInput()
+	{
+		while (true)
+		{
+			cout << "\033[2J\033[1;1H";
+			cout << "Please enter the following data:\n";
+			cout << "Price\n";
+			if (cin >> this->price)
+				break;
+			else
+			{
+				cout << "\033[2J\033[1;1H" << "Incorrect data! Try again...";
+				_getch();
+				cin.clear();
+				cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
+			}
+		}
+		cin.clear();
+		cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
+		while (true)
+		{
+			cout << "\033[2J\033[1;1H";
+			cout << "Please enter the following data:\n";
+			cout << "Brand\n";
+			if (cin.getline(this->brand, 15))
+			{
+				break;
+			}
+			else
+			{
+				cout << "\033[2J\033[1;1H" << "Incorrect data! Try again...";
+				_getch();
+				cin.clear();
+			}
+		}
+		while (true)
+		{
+			cout << "\033[2J\033[1;1H";
+			cout << "Please enter the following data:\n";
+			cout << "Amount of RAM\n";
+			if (cin >> this->ram)
+				break;
+			else
+			{
+				cout << "\033[2J\033[1;1H" << "Incorrect data! Try again...";
+				_getch();
+				cin.clear();
+				cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
+			}
+		}
+		cin.clear();
+		cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
+		while (true)
+		{
+			cout << "\033[2J\033[1;1H";
+			cout << "Please enter the following data:\n";
+			cout << "Processor\n";
+			if (cin.getline(this->proc, 25))
+			{
+				break;
+			}
+			else
+			{
+				cout << "\033[2J\033[1;1H" << "Incorrect data! Try again...";
+				_getch();
+				cin.clear();
+			}
+		}
+		cin.clear();
+		while (true)
+		{
+			cout << "\033[2J\033[1;1H";
+			cout << "Please enter the following data:\n";
+			cout << "Amount of memory\n";
+			if (cin >> this->memory)
+				break;
+			else
+			{
+				cout << "\033[2J\033[1;1H" << "Incorrect data! Try again...";
+				_getch();
+				cin.clear();
+				cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
+			}
+		}
+		cin.clear();
+		while (true)
+		{
+			cout << "\033[2J\033[1;1H";
+			cout << "Please enter the following data:\n";
+			cout << "Count of memory storages\n";
+			if (cin >> this->memory_count)
+				break;
+			else
+			{
+				cout << "\033[2J\033[1;1H" << "Incorrect data! Try again...";
+				_getch();
+				cin.clear();
+				cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
+			}
+		}
+	}
 };
+
 
 // Динамический список
 class List                 // список
 {
-private:
-	Computer* head,* tail; //"голова" связанного списка
 public:
-	List() //конструктор класса без параметров
+	int count = 0;			
+	Computer* head, * tail; //начало и конец списка
+public:
+	//конструктор класса без параметров
+	List() 
 	{
 		head = NULL; //первого элемента пока нет
 		tail = NULL;
 	}
+	//Добавление элемента в начало списка
 	Computer* push_front(Computer comp) {
 		Computer* item = (Computer*)(malloc(sizeof(Computer)));
-		strcpy(item->brand, comp.brand);     //задаем узлу данные		
+		item->prev = item->next = NULL;				//задаем узлу данные	
+		strcpy(item->brand, comp.brand);     	
 		strcpy(item->proc, comp.proc);
 		item->ram = comp.ram;
 		item->memory = comp.memory;
@@ -46,13 +153,13 @@ public:
 		if (tail == NULL)
 			tail = item;
 		head = item;
-
+		count++;
 		return item;
 	}
-
-
+	//Добавление элемента в конец списка
 	Computer* push_back(Computer comp) {
 		Computer* item = (Computer*)(malloc(sizeof(Computer)));
+		item->prev = item->next = NULL;
 		strcpy(item->brand, comp.brand);     //задаем узлу данные		
 		strcpy(item->proc, comp.proc);
 		item->ram = comp.ram;
@@ -65,115 +172,127 @@ public:
 		if (head == NULL)
 			head = item;
 		tail = item;
-
+		count++;
 		return item;
 	}
-	//метод, добавляющий новый узел в список
-	void addNode(Computer comp)
-	{
-		Computer* item = (Computer*)(malloc(sizeof(Computer))); //динамически создаем новый узел		
-		strcpy(item->brand, comp.brand);     //задаем узлу данные		
-		strcpy(item->proc, comp.proc);
-		item->ram = comp.ram;
-		item->memory = comp.memory;
-		item->memory_count = comp.memory_count;
-		item->price = comp.price;
-		item->next = NULL;     //новый узел в конце, поэтому NULL
-		if (head == NULL)     //если создаем первый узел
-			head = item;
-		else                 //если узел уже не первый
-		{
-			Computer* current = head;
-			//ищем в цикле предшествующий последнему узел
-			while (current->next != NULL)
-				current = current->next;
-			//предшествующий указывает на последний
-			current->next = item;
-		}
+
+	//Удаление первого элемента списка
+	void pop_front() {
+		if (head == NULL) return;
+
+		Computer* ptr = head->next;
+		if (ptr != NULL)
+			ptr->prev = NULL;
+		else
+			tail = NULL;
+
+		free(head);
+		head = ptr;
+		count--;
 	}
-	void Clear()
-	{
-		Computer* current = this->head;
-		while (current != nullptr) {
-			Computer* nextNode = current->next; // Сохраняем следующий
-			delete current;                 // Удаляем текущий
-			current = nextNode;             // Переходим к следующему
-		}
-		this->head = nullptr; // Список пуст
+	//Удаление последнего элемента списка
+	void pop_back() {
+		if (tail == NULL) return;
+
+		Computer* ptr = tail->prev;
+		if (ptr != NULL)
+			ptr->next = NULL;
+		else
+			head = NULL;
+
+		free(tail);
+		tail = ptr;
+		count--;
 	}
-	//метод, выводящий связанный список на экран
-	void printList()
+	//Очистка списка
+	~List()
 	{
-		Computer* current = head;
+		while (head != NULL)
+			pop_front();
+		count = 0;
+	}
+	//Вывод списка на экран	
+	void Show()
+	{
 		int i = 1;
-		while (current != NULL)
+		cout << "\033[2J\033[1;1H";
+		cout.width(4);
+		cout.fill(' ');
+		cout << "N";
+		cout.width(15);
+		cout.fill(' ');
+		cout << "Manufacturer";
+		cout.width(25);
+		cout.fill(' ');
+		cout << "Processor";
+		cout.width(10);
+		cout.fill(' ');
+		cout << "RAM";
+		cout.width(15);
+		cout.fill(' ');
+		cout << "Memory";
+		cout.width(20);
+		cout.fill(' ');
+		cout << "Memory count";
+		cout.width(15);
+		cout.fill(' ');
+		cout << "Price" << "\n";
+		for (Computer* ptr = this->head; ptr != NULL; ptr = ptr->next)
 		{
 			cout.width(4);
 			cout.fill(' ');
 			cout << i << ')';
 			cout.width(15);
 			cout.fill(' ');
-			cout << current->brand;
+			cout << ptr->brand;
 			cout.width(25);
 			cout.fill(' ');
-			cout << current->proc;
+			cout << ptr->proc;
 			cout.width(10);
 			cout.fill(' ');
-			cout << current->ram;
+			cout << ptr->ram;
 			cout.width(15);
 			cout.fill(' ');
-			cout << current->memory;
+			cout << ptr->memory;
 			cout.width(20);
 			cout.fill(' ');
-			cout << current->memory_count;
+			cout << ptr->memory_count;
 			cout.width(15);
 			cout.fill(' ');
-			cout << current->price << "\n";
+			cout << ptr->price << "\n";
 			i++;
-			current = current->next;
 		}
 	}
-};    // Динамический список
-// Перечисление для функции записи
+	//Получение элемента списка по индексу
+	Computer* getAt(int index) 
+	{
+		Computer* ptr = head;
+		int i = 0;
+		if (index > this->count / 2)
+		{
+		while (i != index) 
+		{
+			if (ptr == NULL)
+				return ptr;
+			ptr = ptr->next;
+			i++;
+		}
+		}
+
+		return ptr;
+	}
+
+
+	Computer* operator [] (int index) {
+		return getAt(index);
+	}
+};
+
+//Перечисление для функции записи
 enum class Option { Write, Delete, Edit };
-// Переменная для подсчёта кол-ва компов в файле
-int computerCount;
-// ФУНКЦИЯ ЗАПИСИ СТРУКТУРЫ В ФАЙЛ
-void _writeStruct(Computer computer, Option option, int i = NULL)
-{
-	FILE* f; // переменная для работы с файлом
-	switch (option)
-	{
-	case Option::Write:
-	{
-		//E:\\data.bin
-		f = fopen("data.bin", "ab+"); // открываем бинарный файл для записи и чтения в режиме добавления, то есть, если файла нет, то он создастся, а если файл есть, 
-		// то содержимое файла не будет уничтожено, из файла можно будет читать и в файл можно будет записывать				
-		fwrite(&computer, sizeof(Computer), 1, f); // записываем в файл f р		
-		fclose(f); // закрываем файл
-		cout << "\033[2J\033[1;1H";
-		cout << "\nNew computer sucсessfully added!\n\nPress any key to continue...";
-		_getch(); // ожидаем нажатия пользователем клавиши
-	}
-	case Option::Delete:
-	{
-		f = fopen("dataNew.bin", "ab+");
-		fwrite(&computer, sizeof(Computer), 1, f); // записываем в файл f р		
-		fclose(f); // закрываем файл		
-	}
-	case Option::Edit:
-	{
-		f = fopen("data.bin", "r+");  // r+ для изменения файла
-		fseek(f, i * sizeof(Computer), SEEK_SET);
-		fwrite(&computer, sizeof(Computer), 1, f); // записываем в файл f р		
-		fclose(f); // закрываем файл		
-	}
-	}
-}
-// ФУНКЦИЯ ЧТЕНИЯ ИЗ ФАЙЛА
+//Чтение структуры из файла
 void _readStruct(List& list)
 {
-	computerCount = 0;
 	struct Computer* computer = (struct Computer*)malloc(sizeof(Computer));
 	FILE* file; // переменная для работы с файлом
 	long i = 0, fEnd;    // переменная для обозначения конца файла
@@ -187,7 +306,7 @@ void _readStruct(List& list)
 			fseek(file, i, SEEK_SET); // перемещаемся от начала (SEEK_SET) файла на ... длинн структуры
 			fread(computer, sizeof(Computer), 1, file); // считываем из файла f ровно 1 структуру размера Computer			
 			// вывод на консоль загруженной структуры
-			list.addNode(*computer);
+			list.push_back(*computer);
 			i += sizeof(Computer);
 		}
 		fclose(file); // закрываем файл
@@ -196,7 +315,41 @@ void _readStruct(List& list)
 	else
 		throw 404;
 }
-// ФУНКЦИЯ УДАЛЕНИЯ ПК ИЗ ФАЙЛА
+//Запись структуры в файл
+void _writeStruct(Computer computer, Option option, int i = NULL, List list = List())
+{
+	FILE* f; // переменная для работы с файлом
+	switch (option)
+	{
+	case Option::Write:
+
+		//E:\\data.bin
+		f = fopen("data.bin", "ab+"); // открываем бинарный файл для записи и чтения в режиме добавления, то есть, если файла нет, то он создастся, а если файл есть, 
+		fseek(f, 0, SEEK_END);// то содержимое файла не будет уничтожено, из файла можно будет читать и в файл можно будет записывать				
+		fwrite(&computer, sizeof(Computer), 1, f); // записываем в файл f р		
+		fclose(f); // закрываем файл
+		cout << "\033[2J\033[1;1H";
+		cout << "\nNew computer sucсessfully added!\n\nPress any key to continue...";
+		_getch(); // ожидаем нажатия пользователем клавиши
+		break;
+	case Option::Delete:
+		f = fopen("data.bin", "wb");
+		for (Computer* ptr = list.head; ptr != NULL; ptr = ptr->next)
+		{
+			fwrite(ptr, sizeof(Computer), 1, f); // записываем в файл f р		
+		}
+		fclose(f); // закрываем файл		
+		break;
+	case Option::Edit:
+		f = fopen("data.bin", "r+");  // r+ для изменения файла
+		fseek(f, i * sizeof(Computer), SEEK_SET);
+		fwrite(&computer, sizeof(Computer), 1, f); // записываем в файл f р		
+		fclose(f); // закрываем файл		
+		break;
+	}
+
+}
+//Удаление структуры из файла
 void _delStruct(int number)
 {
 	struct Computer* computer = (struct Computer*)malloc(sizeof(Computer));
@@ -230,7 +383,7 @@ void _delStruct(int number)
 int main()
 {
 	List list;
-	int menu;
+	int menu, i;
 	menu = NULL;
 	Computer computer;
 	try
@@ -239,200 +392,61 @@ int main()
 	}
 	catch (int code)
 	{
-		while (menu != 49 || menu != 50)
+		while (true)
 		{
 			cout << "\033[2J\033[1;1H";
 			cout << "Error 404\n\nComputers not found\n\n";
 			cout << "1.Add computer\n";
-			cout << "2. Exit\n";
+			cout << "2.Exit\n";
 			menu = _getch();
+			if (menu == 50)
+			{
+				menu = 54;
+				break;
+			}
+			else if (menu == 49)
+			{
+				menu = 50;
+				break;
+			}
 		}
 	}
 	while (menu != 54)
 	{
-		cout << "\033[2J\033[1;1H"; // Console clear and start from top left of window
-		cout << "Choose option!\n\n";
-		cout << "1.Computer list\n";
-		cout << "2.Add computer\n";
-		cout << "3.Edit computer\n";
-		cout << "4.Delete computer\n";
-		cout << "5.\n";
-		cout << "6. Exit\n";
-		menu = _getch();
+		if (menu != 50)
+		{
+			cout << "\033[2J\033[1;1H"; // Console clear and start from top left of window
+			cout << "Choose option!\n\n";
+			cout << "1.Computer list\n";
+			cout << "2.Add computer\n";
+			cout << "3.Edit computer\n";
+			cout << "4.Delete computer\n";
+			cout << "5.\n";
+			cout << "6. Exit\n";
+			menu = _getch();
+		}
 		switch (menu)
 		{
 		case 49:																	// SHOW LIST
 			menu = NULL;
-			cout << "\033[2J\033[1;1H";
-			cout.width(4);
-			cout.fill(' ');
-			cout << "N";
-			cout.width(15);
-			cout.fill(' ');
-			cout << "Manufacturer";
-			cout.width(25);
-			cout.fill(' ');
-			cout << "Processor";
-			cout.width(10);
-			cout.fill(' ');
-			cout << "RAM";
-			cout.width(15);
-			cout.fill(' ');
-			cout << "Memory";
-			cout.width(20);
-			cout.fill(' ');
-			cout << "Memory count";
-			cout.width(15);
-			cout.fill(' ');
-			cout << "Price" << "\n";
-			list.printList();
+			list.Show();
 			cout << "Press any button to exit...";
 			_getch();
 			break;
-		case 50:															// ADD
+		case 50:																	// ADD
 			menu = NULL;
-			while (true)
-			{
-				cout << "\033[2J\033[1;1H";
-				cout << "Please enter the following data:\n";
-				cout << "Brand\n";
-				if (cin.getline(computer.brand, 15))
-				{
-					break;
-				}
-				else
-				{
-					cout << "\033[2J\033[1;1H" << "Incorrect data! Try again...";
-					_getch();
-					cin.clear();
-				}
-			}
-			while (true)
-			{
-				cout << "\033[2J\033[1;1H";
-				cout << "Please enter the following data:\n";
-				cout << "Price\n";
-				if (cin >> computer.price)
-					break;
-				else
-				{
-					cout << "\033[2J\033[1;1H" << "Incorrect data! Try again...";
-					_getch();
-					cin.clear();
-					cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
-				}
-			}
-			cin.clear();
-			cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
-			while (true)
-			{
-				cout << "\033[2J\033[1;1H";
-				cout << "Please enter the following data:\n";
-				cout << "Processor\n";
-				if (cin.getline(computer.proc, 25))
-				{
-					break;
-				}
-				else
-				{
-					cout << "\033[2J\033[1;1H" << "Incorrect data! Try again...";
-					_getch();
-					cin.clear();
-				}
-			}
-			// cin.clear();
-			while (true)
-			{
-				cout << "\033[2J\033[1;1H";
-				cout << "Please enter the following data:\n";
-				cout << "Amount of RAM\n";
-				if (cin >> computer.ram)
-					break;
-				else
-				{
-					cout << "\033[2J\033[1;1H" << "Incorrect data! Try again...";
-					_getch();
-					cin.clear();
-					cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
-				}
-			}
-			cin.clear();
-			while (true)
-			{
-				cout << "\033[2J\033[1;1H";
-				cout << "Please enter the following data:\n";
-				cout << "Amount of memory\n";
-				if (cin >> computer.memory)
-					break;
-				else
-				{
-					cout << "\033[2J\033[1;1H" << "Incorrect data! Try again...";
-					_getch();
-					cin.clear();
-					cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
-				}
-			}
-			cin.clear();
-			while (true)
-			{
-				cout << "\033[2J\033[1;1H";
-				cout << "Please enter the following data:\n";
-				cout << "Count of memory storages\n";
-				if (cin >> computer.memory_count)
-					break;
-				else
-				{
-					cout << "\033[2J\033[1;1H" << "Incorrect data! Try again...";
-					_getch();
-					cin.clear();
-					cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
-				}
-			}
+			computer.CompInput();
 			_writeStruct(computer, Option::Write);
-			list.Clear();
+			list.~List();
 			_readStruct(list);
 			break;
 		case 51:																	// EDIT
 			menu = NULL;
 			while (true)
 			{
-				cout << "\033[2J\033[1;1H";
-
-				cout.width(4);
-				cout.fill(' ');
-				cout << "N";
-				cout.width(15);
-				cout.fill(' ');
-				cout << "Manufacturer";
-				cout.width(15);
-				cout.fill(' ');
-				cout << "Processor";
-				cout.width(10);
-				cout.fill(' ');
-				cout << "RAM";
-				cout.width(15);
-				cout.fill(' ');
-				cout << "Memory";
-				cout.width(20);
-				cout.fill(' ');
-				cout << "Memory count";
-				cout.width(15);
-				cout.fill(' ');
-				cout << "Price" << "\n";
-				try
-				{
-					_readStruct(list);
-				}
-				catch (int code)
-				{
-					cout << "\033[2J\033[1;1H";
-					cout << "Error 404\n\nComputers not found\n\n";
-					cout << "Press any button to exit...";
-					_getch();
-					break;
-				}
-				cout << "Enter whitch computer you want to edit:\n";
-				if (cin >> menu && menu <= computerCount && menu != 0)
+				list.Show();
+				cout << "Enter which computer you want to edit:\n";
+				if (cin >> menu && menu <= list.count && menu > 0)
 					break;
 				else
 				{
@@ -442,122 +456,21 @@ int main()
 					cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
 				}
 			}
-			cout << "\033[2J\033[1;1H";
-			cout << "Please enter the following data:\n";
-			cout << "Brand\n";
-			cin >> computer.brand;
-			while (true)
-			{
-				cout << "\033[2J\033[1;1H";
-				cout << "Please enter the following data:\n";
-				cout << "Price\n";
-				if (cin >> computer.price)
-					break;
-				else
-				{
-					cout << "\033[2J\033[1;1H" << "Incorrect data! Try again...";
-					_getch();
-					cin.clear();
-					cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
-				}
-			}
-			cout << "\033[2J\033[1;1H";
-			cout << "Please enter the following data:\n";
-			cout << "Processor\n";
-			cin >> computer.proc;
-			while (true)
-			{
-				cout << "\033[2J\033[1;1H";
-				cout << "Please enter the following data:\n";
-				cout << "Amount of RAM\n";
-				if (cin >> computer.ram)
-					break;
-				else
-				{
-					cout << "\033[2J\033[1;1H" << "Incorrect data! Try again...";
-					_getch();
-					cin.clear();
-					cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
-				}
-			}
-			while (true)
-			{
-				cout << "\033[2J\033[1;1H";
-				cout << "Please enter the following data:\n";
-				cout << "Amount of memory\n";
-				if (cin >> computer.memory)
-					break;
-				else
-				{
-					cout << "\033[2J\033[1;1H" << "Incorrect data! Try again...";
-					_getch();
-					cin.clear();
-					cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
-				}
-			}
-			while (true)
-			{
-				cout << "\033[2J\033[1;1H";
-				cout << "Please enter the following data:\n";
-				cout << "Count of memory storages\n";
-				if (cin >> computer.memory_count)
-					break;
-				else
-				{
-					cout << "\033[2J\033[1;1H" << "Incorrect data! Try again...";
-					_getch();
-					cin.clear();
-					cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
-				}
-			}
+			computer.CompInput();
 			_writeStruct(computer, Option::Edit, menu - 1);
-			free(&list);
+			list.~List();
 			_readStruct(list);
 			menu = NULL;
 			cout << "Press any button to exit...";
 			_getch();
 			break;
-		case 52:																		// DELETE
+		case 52:																	// DELETE
 			menu = NULL;
 			while (true)
 			{
-				cout << "\033[2J\033[1;1H";
-
-				cout.width(4);
-				cout.fill(' ');
-				cout << "N";
-				cout.width(15);
-				cout.fill(' ');
-				cout << "Manufacturer";
-				cout.width(25);
-				cout.fill(' ');
-				cout << "Processor";
-				cout.width(10);
-				cout.fill(' ');
-				cout << "RAM";
-				cout.width(15);
-				cout.fill(' ');
-				cout << "Memory";
-				cout.width(20);
-				cout.fill(' ');
-				cout << "Memory count";
-				cout.width(15);
-				cout.fill(' ');
-				cout << "Price" << "\n";
-				try
-				{
-					_readStruct(list);
-				}
-				catch (int code)
-				{
-					cout << "\033[2J\033[1;1H";
-					cout << "Error 404\n\nComputers not found\n\n";
-					cout << "Press any button to exit...";
-					_getch();
-					break;
-				}
-				cout << "Enter whitch computer you want to destroy:\n";
-				if (cin >> menu && menu <= computerCount && menu != 0)
+				list.Show();
+				cout << "Enter which computer you want to destroy:\n";
+				if (cin >> menu && menu <= list.count && menu > 0)
 					break;
 				else
 				{
@@ -568,7 +481,7 @@ int main()
 				}
 			}
 			_delStruct(menu - 1);
-			free(&list);
+			list.~List();
 			_readStruct(list);
 			menu = NULL;
 			cout << "Press any button to exit...";
